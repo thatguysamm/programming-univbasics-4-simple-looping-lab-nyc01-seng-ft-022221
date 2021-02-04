fe112f0bdfa04e08403fee79d232f562ee6087e4
friendly-format-0074 <samuelagasi@gmail.com>
2021-02-04 17:19:44 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,8 +1,8 @@
-def loop_message_five_times(message)
+def loop_message_five_times(string)
   count= 0
   while count< 5 do
     puts "Hello World."
     count=+1
   end
 end
-loop_message_five_times(message)
+loop_message_five_times(string)
